39f39996e073e560c6ac0f737256bc47e58dfde9
eugenel97 <eugenejr14@gmail.com>
2025-04-17 10:34:58 -0700

    Add adventure picker functionality to App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <h1>Solo Adventure Picker</h1>
+    <button @click="getRandomAdventure">Pick an Adventure</button>
+    <div v-if="adventure" class="card">
+      <h2>{{ adventure }}</h2>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup>
+import { ref } from 'vue'
+
+const adventure = ref('')
+
+const getRandomAdventure = async () => {
+  const res = await fetch('http://localhost:8080/random')
+  const data = await res.json()
+  adventure.value = data.Name
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
