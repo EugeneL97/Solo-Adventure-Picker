421b9afd4060f8abdaf93ed5478981d707576da9
Eugene L <eugenejr14@gmail.com>
2025-04-23 19:08:13 -0700

    Add AdventurePage.vue to display random solo adventures

---
+++ frontend/src/views/AdventurePage.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <h1>Solo Adventure Picker</h1>
+
+    <div v-if="adventure" class="card">
+      <h2>{{ displayName }}</h2>
+      <h2>{{ displayType }}</h2>
+      <!-- <p v-if="adventure.tags?.length">Tags: {{ adventure.tags.join(', ') }}</p> -->
+    </div>
+
+    <p v-if="errorMsg" class="error">{{ errorMsg }}</p>
+
+    <button @click="getRandomAdventure">Pick Another Adventure</button>
+  </div>
+</template>
+
+<script setup>
+import { capitalizeWords } from "../utils/formatting.js";
+import {ref, onMounted, computed} from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const adventure = ref(null)
+const errorMsg = ref('')
+
+const getRandomAdventure = async () => {
+  const region = route.query.region || ''
+  try {
+    const res = await fetch(`http://localhost:8080/random?region=${region}`)
+    const data = await res.json()
+
+    if (!res.ok) throw new Error(data.error || 'Something went wrong')
+
+    adventure.value = data
+    errorMsg.value = ''
+  } catch (err) {
+    adventure.value = null
+    errorMsg.value = err.message
+  }
+}
+
+onMounted(getRandomAdventure)
+
+const displayName = computed(() => capitalizeWords(adventure.value?.name))
+const displayType = computed(() => capitalizeWords(adventure.value?.type))
+</script>
+
+<style scoped>
+.error {
+  color: red;
+  margin-top: 1rem;
+}
+.card {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  margin-top: 1rem;
+}
+</style>
